f7a773e4411912967400cb0c82fc24c19b70c6e9
sondeokhyeon <sondeokhyeon@gmail.com>
2019-05-08 17:03:54 +0900

    Add: Today I Learn

--- server/contollers/userController.js
+++ server/contollers/userController.js
@@ -1,7 +1,20 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const users = (req, res) => res.render("users");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) =>
+    res.render("join", { pageTitle: "join" });
+
+export const login = (req, res) =>
+    res.render("login", { pageTitle: "login" });
+
+export const logout = (req, res) =>
+    res.render("logout", { pageTitle: "logout" });
+
+export const users = (req, res) =>
+    res.render("users", { pageTitle: "users" });
+
+export const userDetail = (req, res) =>
+    res.render("userDetail", { pageTitle: "userDetail" });
+
+export const editProfile = (req, res) =>
+    res.render("editProfile", { pageTitle: "editProfile" });
+
+export const changePassword = (req, res) =>
+    res.render("changePassword", { pageTitle: "changePassword" });
--- server/contollers/videoController.js
+++ server/contollers/videoController.js
@@ -1,21 +1,26 @@
+import { videos } from "../db"
+
 export const home = (req, res) =>
-    res.render("home", { pageTitle: "Home" }
+    res.render("home", { pageTitle: "Home", videos: videos }
     );
-export const search = (req, res) =>
-    res.render("search", { pageTitle: "Search" }
-    );
-export const videos = (req, res) =>
-    res.render("videos", { pageTitle: "Videos" }
-    );
+
+export const search = (req, res) => {
+    const { query: { term: searchingBy } } = req;
+    res.render("search", { pageTitle: "Search", searchingBy: searchingBy })
+};
+
 export const upload = (req, res) =>
     res.render("upload", { pageTitle: "Upload" }
     );
+
 export const videoDetail = (req, res) =>
     res.render("videioDetail", { pageTitle: "Videio Detail" }
     );
+
 export const editVideo = (req, res) =>
     res.render("editVideo", { pageTitle: "Edit Video" }
     );
+
 export const deleteVideo = (req, res) =>
     res.render("deleteVideo", { pageTitle: "Delete Video" }
     );
