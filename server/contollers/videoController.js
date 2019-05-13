7db7c534bbf5d0c1b6d334676d78e49182de30fa
손덕현 <sondeokhyeon@gmail.com>
2019-05-13 23:05:46 +0900

    Today I Learn

--- server/contollers/videoController.js
+++ server/contollers/videoController.js
@@ -17,15 +17,19 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", {
-    pageTitle: "Search",
-    searchingBy,
-    videos
-  });
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const getUpload = (req, res) =>
