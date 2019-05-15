a517d78bee2f475ed19fcd9d1e24f4b28fae6913
sondeokhyeon <sondeokhyeon@gmail.com>
2019-05-15 16:39:19 +0900

    Today I Learn

--- server/app.js
+++ server/app.js
@@ -1,38 +1,36 @@
-//const express = require('express')
+// const express = require('express')
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParse from "cookie-parser";
 import bodyParse from "body-parser";
-import userRouter from "./router/userRouter"
-import videoRouter from "./router/videoRouter"
-import globalRouter from "./router/globalRouter"
-import routes from "./routes"
-import {
-    localsMiddleware
-} from "./middlewares";
+import userRouter from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
+import globalRouter from "./router/globalRouter";
+import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
-const app = express()
+const app = express();
 
 app.use(helmet()); // 보안   middleware
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"))
-app.use(cookieParse()); // cookie관련 middleware 쿠키 사용을 도와줌 
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
+app.use(cookieParse()); // cookie관련 middleware 쿠키 사용을 도와줌
 app.use(bodyParse.json()); // req, res 정보들을 검사 하는 middleware
-app.use(bodyParse.urlencoded({
+app.use(
+  bodyParse.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(morgan("dev")); // logger middleware
-app.use(localsMiddleware)
-
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter); // 라우터 등록
-app.use(routes.videos, videoRouter); // 라우터 사용 
+app.use(routes.videos, videoRouter); // 라우터 사용
 
 export default app;
-
-
 
 /*
 const handlehome = (req, res) => res.send('Hello1 From Home!');
